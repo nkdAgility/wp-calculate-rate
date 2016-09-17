5e3bca5dc9f6da786f1fcf0607cbffeedfdcab41
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2016-09-17 15:11:31 +0100

    Fix from Kirsty Miller

--- wp-calculate-rate.php
+++ wp-calculate-rate.php
@@ -1,20 +1,30 @@
 <?php
- 
-/*
+/**
 * Plugin Name: nkdAgility Rate Calculator
 * Description: Tell Us What Your Shortcode Does
 * Version: 0.6
 * Author: MrHinsh
 * Author URI: https://nkdagility.com
 */
-function nkdCalculate_shortcode($atts) {
-    $result = wpv_do_shortcode($atts);
-    $result = eval("return $result;");
-    return $result;
+
+function nkdCalculate_shortcodes_init() {
+
+    function nkdCalculate_shortcode($atts,$content=null) { 
+        $content = wpv_do_shortcode($content);
+        $content = eval("return $content;");
+        return $content;
+    }
+
+    add_shortcode('nkd-calculate', 'nkdCalculate_shortcode');
+
+    function nkdCalculate_rate_shortcode($atts) {
+    return wget_option('my_rate');
+    }
+
+    add_shortcode('nkd-rate', 'nkdCalculate_rate_shortcode');
+
 }
-add_shortcode('nkd-calculate', 'nkdCalculate_shortcode');
-function nkdCalculate_rate_shortcode($atts) {
-   return wget_option('my_rate');
-}
-add_shortcode('nkd-rate', 'nkdCalculate_rate_shortcode');
+
+add_action('init', 'nkdCalculate_shortcodes_init');
+
 ?>
